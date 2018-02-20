c44bb14acbc7dea842775b45423a55edfbaec53c
Leonard MARQUES <lmarques@student.42.fr>
2018-02-20 19:07:45 +0100

    fixed ft_isstrdigit

--- libft/ft_isstrdigit.c
+++ libft/ft_isstrdigit.c
@@ -6,7 +6,7 @@
 /*   By: lmarques <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/20 18:33:42 by lmarques          #+#    #+#             */
-/*   Updated: 2018/02/20 18:57:49 by lmarques         ###   ########.fr       */
+/*   Updated: 2018/02/20 19:06:47 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ int	ft_isstrdigit(const char *s)
 {
 	int	count;
 
-	count = 0;
-	while (s[count])
+	count = -1;
+	while (s[++count])
 		if (!ft_isdigit(s[count]))
 			return (0);
 	return (1);
