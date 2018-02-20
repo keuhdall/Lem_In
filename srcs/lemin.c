c5a1c3b47908f708aecbeac4c2ff4557eea7ef80
Leonard MARQUES <lmarques@student.42.fr>
2018-02-20 19:17:44 +0100

    added simple main for tests purpose

--- srcs/lemin.c
+++ srcs/lemin.c
@@ -6,7 +6,7 @@
 /*   By: lmarques <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/09 17:55:10 by lmarques          #+#    #+#             */
-/*   Updated: 2018/02/13 18:42:22 by lmarques         ###   ########.fr       */
+/*   Updated: 2018/02/20 19:08:41 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,13 @@
 
 int	main(int argc, char *argv[])
 {
-	if (argc)
-		(void)argv;
+	t_env env;
+
+	ft_bzero(&env, sizeof(env));
+	if (argc != 2)
+		puterr(ERR_WRONG_ARGS);
+	init_structs(argv[1], &env);
+	ft_putendl("test");
+	read_file(argv[1], &env);
 	return (0);
 }
