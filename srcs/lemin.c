8b6068944c07a1f9434adb56d80badfe8b783c2f
Leonard MARQUES <lmarques@student.42.fr>
2018-02-21 15:17:08 +0100

    updated main

--- srcs/lemin.c
+++ srcs/lemin.c
@@ -6,13 +6,28 @@
 /*   By: lmarques <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/09 17:55:10 by lmarques          #+#    #+#             */
-/*   Updated: 2018/02/21 14:31:02 by lmarques         ###   ########.fr       */
+/*   Updated: 2018/02/21 15:16:29 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/lemin.h"
+#include <stdio.h>
 
-int	main(int argc, char *argv[])
+void	print_neighbors(t_room *room)
+{
+	t_room_list	*rl;
+
+	rl = NULL;
+	rl = room->neighbors;
+	printf("Neighbors : \n");
+	while (rl)
+	{
+		printf("%s\n", rl->content.name);
+		rl = rl->next;
+	}
+}
+
+int		main(int argc, char *argv[])
 {
 	t_env env;
 
@@ -22,7 +37,9 @@ int	main(int argc, char *argv[])
 	init_structs(argv[1], &env);
 	read_file(argv[1], &env);
 	for (int i = 0; i < env.rooms_length; i++) {
-		ft_putendl(env.rooms[i].content.name);
+		printf("%s\n", env.rooms[i].content.name);
+		print_neighbors(&env.rooms[i]);
+		printf("================\n");
 	}
 	return (0);
 }
