044050458e0695e38cba3a3b71dc35d199810808
Léonard Marques <lmarques@student.42.fr>
2018-02-22 18:31:44 +0100

    updated main test

--- srcs/lemin.c
+++ srcs/lemin.c
@@ -6,7 +6,7 @@
 /*   By: lmarques <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/09 17:55:10 by lmarques          #+#    #+#             */
-/*   Updated: 2018/02/21 20:21:45 by lmarques         ###   ########.fr       */
+/*   Updated: 2018/02/22 18:30:16 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,10 +36,15 @@ int		main(int argc, char *argv[])
 		puterr(ERR_WRONG_ARGS);
 	init_structs(argv[1], &env);
 	read_file(argv[1], &env);
+	check_env(&env);
+	fill_ants(&env);
 	for (int i = 0; i < env.rooms_length; i++) {
 		printf("Name : %s\n posX : %d posY: %d\n", env.rooms[i].content.name, env.rooms[i].content.pos.x, env.rooms[i].content.pos.y);
 		print_neighbors(&env.rooms[i]);
 		printf("================\n");
+	}
+	for (int i = 0; i < env.ants_length; i++) {
+		printf("ant ID : %d is in room : %s\n", env.ants[i].id, env.ants[i].room.content.name);
 	}
 	return (0);
 }
