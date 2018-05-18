3fb4d6e93250d754f144a84d6950a045be4550b1
Léonard Marques <lmarques@student.42.fr>
2018-05-18 03:41:18 +0200

    fixed ants index

--- srcs/print.c
+++ srcs/print.c
@@ -6,7 +6,7 @@
 /*   By: lmarques <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/17 23:02:00 by lmarques          #+#    #+#             */
-/*   Updated: 2018/05/17 23:41:08 by lmarques         ###   ########.fr       */
+/*   Updated: 2018/05/18 03:40:16 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	default_print(t_env *env, int farest, int count)
 {
-	ft_printf("L%d-%s ", farest + count,
+	ft_printf("L%d-%s ", farest + count + 1,
 		get_next_room(env, env->ants[farest + count])->name);
 }
 
